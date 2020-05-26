f04de92dbe9359a950d4c8e108cd6894d2cd88be
jerry <jerry@aa.com>
2020-05-26 20:09:20 +0800

    chatroom-0.0.2,新增发送图片功能

--- src/main/java/com/zjc/chatroom/controller/ChatController.java
+++ src/main/java/com/zjc/chatroom/controller/ChatController.java
@@ -18,7 +18,7 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Controller
-public class MyChatController {
+public class ChatController {
 
     private MessageEntity messageEntity=new MessageEntity();
 
